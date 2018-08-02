5cb91d3de8de469e37734347aef5ffef895a7305
vlad <vladaavekin@gmail.com>
2018-08-02 16:04:26 +0300

    fix migration DB

--- src/main/resources/db.migration/V1__init_DB.sql
+++ src/main/resources/db.migration/V1__init_DB.sql
@@ -1,33 +1,33 @@
-create sequence hibernate_sequence start with 1 increment by 1;
+create sequence hibernate_sequence start with 1 increment by 1
 
 create table message (
-    id int8 not null,
-    filename varchar(255),
-    tag varchar(255),
-    text varchar(255) not null,
-    user_id bigint,
-    primary key (id)
+  id bigint not null,
+  filename varchar(255),
+  tag varchar(255),
+  text varchar(255),
+  user_id bigint,
+  primary key (id)
 );
 
 create table user_role (
-    user_id bigint not null,
-    roles varchar(255)
+  user_id bigint not null,
+  roles varchar(255)
 );
 
 create table usr (
-    id int8 not null,
-    activation_code varchar(255),
-    active boolean not null,
-    email varchar(255),
-    password varchar(255) not null,
-    username varchar(255) not null,
-    primary key (id)
+  id bigint not null,
+  activation_code varchar(255),
+  active boolean not null,
+  email varchar(255),
+  password varchar(255),
+  username varchar(255),
+  primary key (id)
 );
 
 alter table message
-    add constraint message_user_fk
-    foreign key (user_id) references usr
+  add constraint FK70bv6o4exfe3fbrho7nuotopf
+  foreign key (user_id) references usr
 
 alter table user_role
-    add constraint user_role_user_fk
-    foreign key (user_id) references usr
+  add constraint FKfpm8swft53ulq2hl11yplpr5
+  foreign key (user_id) references usr
--- src/main/resources/db.migration/V2__Add_admin.sql
+++ src/main/resources/db.migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
-insert into user (id, username, password, active)
-    value (1, 'admin', '123', true);
+insert into usr (id, username, password, active)
+    values (1, 'admin', '123', true);
 
-insert into user_role (user_id, role)
-    value (1, 'USER'), value (1, 'ADMIN');
+insert into user_role (user_id, roles)
+    values (1, 'USER'), (1, 'ADMIN');
